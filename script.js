9f7989682334263682d104a6af0751cf52f301ec
kat2799 <79150216+kat2799@users.noreply.github.com>
2021-10-16 17:19:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
-class Calculator
-{ 
-	var Fcalc = document.calc;
-var Flag = false;
-var Now = 0;
-var PendingOp = "";
+ // получить элемент
+  var el = function(element) {
+    if (element.charAt(0) === "#") { 
+      return document.querySelector(element);
+    }
+    return document.querySelectorAll(element);  
+  };
 
-// нажатие кнопки
-function NumPressed (Num) 
-{
-		if (Flag) 
-		{
-			Fcalc.ReadOut.value = Num; 
-			Flag = false;
-		}	
-		else 
-		{
-			if (Fcalc.ReadOut.value == "0")
-				Fcalc.ReadOut.value = Num;
-			else
-				Fcalc.ReadOut.value += Num;
-		}
-}
-// обработчик нажатия кнопки действия
-function Operation (Op) 
-{
-		var Readout = Fcalc.ReadOut.value;
-		if (Flag && PendingOp != "=")
-		{
-			Fcalc.ReadOut.value = Currents;
-		}
-		else
-		{
-			 Flag = true;
-			if ( '+' == PendingOp )
-				Now += parseFloat(Readout);
-			else if ( '-' == PendingOp )
-				Now -= Readout;
-			else if ( '/' == PendingOp )
-				Now /= Readout;
-			else if ( '*' == PendingOp )
-				Now *= Readout;
-			else
-				Now = parseFloat(Readout); 
-           
-			Fcalc.ReadOut.value = Now;
-			PendingOp = Op;
-		}
-}
-	
-// удаление последнего числа 
-function ClearEntry () 
-{
- var curReadOut = Fcalc.ReadOut.value;
-		curReadOut = curReadOut.toString().slice(0,-1);
-        Fcalc.ReadOut.value=curReadOut;
-		Flag = true;
-}
-	
-// Полная очистка всех результатов
-function Clear () 
-{
-		Now= 0;
-		PendingOp = "";
-		Fcalc.ReadOut.value = "0";
-		FlagNewNum = true;
+  var viewer = el("#viewer"), // окошко с результатом
+    equals = el("#equals"), // кнопки со значениями
+    nums = el(".num"), // список чисел
+    ops = el(".ops"), // список операторов
+    theNum = "", // тек знач
+    oldNum = "", // пред знач
+    resultNum, // результат
+    operator; 
 
-}
-}
+  // при нажатии на число
+  var setNum = function() {
+    if (resultNum) { //если число отображено, удаляет рез-т и присваивает заново
+      theNum = this.getAttribute("data-num");
+      resultNum = "";
+    } else { //добавляет сл число
+      theNum += this.getAttribute("data-num");
+    }
+    viewer.innerHTML = theNum; 
+  };
+  // при нажатии оператора сохраняет стар чис и оператор
+  function moveNum() {
+    oldNum = theNum;
+    theNum = "";
+    operator = this.getAttribute("data-ops");
+    equals.setAttribute("data-result", ""); 
+  };
+  // при нажатии равно происходит подсчет операции
+    function displayNum () {
+    oldNum = parseFloat(oldNum); //превращает стринг в число
+    theNum = parseFloat(theNum);
+    switch (operator) {
+      case "+":
+        resultNum = oldNum + theNum;
+        break;
+      case "-":
+        resultNum = oldNum - theNum;
+        break;
+      case "*":
+        resultNum = oldNum * theNum;
+        break;
+      case "/":
+        resultNum = oldNum / theNum;
+        break; 
+    }
+    viewer.innerHTML = resultNum;    // Отображение рез-та
+    equals.setAttribute("data-result", resultNum);
+    oldNum = 0; // удаляем старое число и присваиваем результат текущему
+    theNum = resultNum;
+  };
+  // полная очистка
+ function clearAll () {
+    oldNum = "";
+    theNum = "";
+    viewer.innerHTML = "0";
+    equals.setAttribute("data-result", resultNum);
+  };
+function deleteAll () //удаляем последнюю цифру
+{
+theNum=theNum.substring(0,theNum.length-1);
+viewer.innerHTML = theNum;
+};
+
+  // добавляем клик на числа
+  for (var i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum;
+  }
+
+  // добавляем клик на знаки
+  for (var i = 0, l = ops.length; i < l; i++) {
+    ops[i].onclick = moveNum;
+  }
